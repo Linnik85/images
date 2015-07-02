0cb794f04ce877c7c5fd1fe567619e7bdb93830b
Linnik85 <apple@Linnik-MacBook-Pro.local>
2015-07-02 11:38:21 +0300

    Initial Commit

---
+++ images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  images
+//
+//  Created by Линник Александр on 02.07.15.
+//  Copyright (c) 2015 Alex Linnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ images/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  images
+//
+//  Created by Линник Александр on 02.07.15.
+//  Copyright (c) 2015 Alex Linnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
